2c71c1ec427237b3b1ad1b606eab7caf6b00435d
apiszczukw <apiszczukw@gmail.com>
2025-05-05 18:15:42 +0200

    Zakładka Projekty oraz O mnie

--- PortfolioKogniLab1/Controllers/HomeController.cs
+++ PortfolioKogniLab1/Controllers/HomeController.cs
@@ -6,8 +6,32 @@ namespace PortfolioKogniLab1.Controllers
 {
     public class HomeController : Controller
     {
-
+        
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("CV")]
+        public IActionResult CV()
+        {
+            return View();
+        }
+
+        [Route("Kontakt")]
+        public IActionResult Kontakt()
+        {
+            return View();
+        }
+
+        [Route("Omnie")]
+        public IActionResult Omnie()
+        {
+            return View();
+        }
+
+        [Route("Projekty")]
+        public IActionResult Projekty()
         {
             return View();
         }
